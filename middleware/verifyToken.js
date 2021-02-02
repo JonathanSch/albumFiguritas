9fdde736d3cf6dbaac7cadb188ddcf997afe3d5a
JonathanSch <jonathansch7@gmail.com>
2021-02-02 11:44:37 -0600

    Things added

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req,res,next) =>{
     try {
+        console.log(req.body.token)
         const verificar = jwt.verify(req.body.token, process.env.JWT_SECRET); 
         req.token = verificar;
 
